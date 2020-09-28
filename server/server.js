2c0c4cb83a4034c9c44bdad973d4fc13fcdf2bac
krizebcev <krizebcev@foi.hr>
2020-09-29 00:21:25 +0200

    Implemented API for restaurants.

--- server/server.js
+++ server/server.js
@@ -6,48 +6,107 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/restaurants", async (req, res) => {
-  const result = await db.query("select * from restaurants");
+  try {
+    const result = await db.query("SELECT * FROM restaurants");
 
-  console.log(result.rows);
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurants: [],
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      results: result.rows.length,
+      data: {
+        restaurants: result.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
-app.get("/api/restaurants/:id", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: {},
-    },
-  });
+app.get("/api/restaurants/:id", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM restaurants WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    if (result.rowCount > 0) {
+      res.status(200).json({
+        status: "success",
+        data: {
+          restaurant: result.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        message: `Restaurant with ID of ${req.params.id} does not exist.`,
+      });
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
-app.post("/api/restaurants", (req, res) => {
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: {},
-    },
-  });
+app.post("/api/restaurants", async (req, res) => {
+  try {
+    const result = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: result.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
-app.put("/api/restaurants/:id", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    data: {
-      restaurant: {},
-    },
-  });
+app.put("/api/restaurants/:id", async (req, res) => {
+  try {
+    const result = await db.query(
+      "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+
+    if (result.rowCount == 1) {
+      res.status(200).json({
+        status: "success",
+        data: {
+          restaurant: result.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        message: `Restaurant with ID of ${req.params.id} does not exist.`,
+      });
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
-app.delete("/api/restaurants/:id", (req, res) => {
-  res.status(204).json({
-    status: "success",
-  });
+app.delete("/api/restaurants/:id", async (req, res) => {
+  try {
+    const result = await db.query("DELETE FROM restaurants WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    if (result.rowCount == 1) {
+      res.status(200).json({
+        status: "success",
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+        message: `Restaurant with ID of ${req.params.id} does not exist.`,
+      });
+    }
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
 const port = process.env.PORT || 3000;
