557f563175b216aa6c8d78968386df9dff8aec98
krizebcev <krizebcev@foi.hr>
2020-10-03 20:12:25 +0200

    Added CORS package to the backend.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const db = require("./db");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/restaurants", async (req, res) => {
