4ad999089c5433fd7b488a87982f1b401e718091
krizebcev <krizebcev@foi.hr>
2020-10-04 22:12:55 +0200

    Created reviews table in the database and deleted products table.

--- DB/db.sql
+++ DB/db.sql
@@ -1,14 +1,4 @@
-DROP TABLE products;
-
-CREATE TABLE products
-(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    price INT NOT NULL,
-    on_sale BOOLEAN NOT NULL
-);
-
-DROP TABLE restaurants;
+DROP TABLE IF EXISTS restaurants;
 
 CREATE TABLE restaurants
 (
@@ -16,4 +6,15 @@ CREATE TABLE restaurants
     name VARCHAR(50) NOT NULL,
     location VARCHAR NOT NULL,
     price_range INT NOT NULL CHECK(price_range >= 1 AND price_range <= 5)
+);
+
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE reviews
+(
+    id SERIAL PRIMARY KEY,
+    restaurant_id INT NOT NULL REFERENCES restaurants (id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL CHECK(rating >= 1 AND rating <= 5)
 );
